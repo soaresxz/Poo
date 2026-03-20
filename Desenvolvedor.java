f6ee67ba6bcb8ee752cfee573a1d6f0eb9cc4ce2
soaresxz <149458343+soaresxz@users.noreply.github.com>
2026-03-19 23:42:01 -0300

    Refactor Desenvolvedor class to extend Funcionarios

--- Desenvolvedor.java
+++ Desenvolvedor.java
@@ -1,27 +1,35 @@
-public class Desenvolvedor {
-    private double bonusSalarial;
+public class Desenvolvedor extends Funcionarios{
     private double horaExtra;
     private double valorHoraExtra;
 
-    public Desenvolvedor(String nome, double horaExtra, double valorHoraExtra){
-        super(nome);
+    public Desenvolvedor(String nome, double salarioBase, double horaExtra, double valorHoraExtra){
+        super(nome, salarioBase); 
         this.horaExtra = horaExtra;
         this.valorHoraExtra = valorHoraExtra;
     }
 
-    public int getHoraExtra(){
+    public double getHoraExtra(){
         return this.horaExtra;
     }
 
-    public void setHoraExtra(int horaExtra){
+    public void setHoraExtra(double horaExtra){
         this.horaExtra = horaExtra;
+    }
+
+    public double getValorHoraExtra(){
+        return this.valorHoraExtra;
+    }
+
+    public void setValorHoraExtra(double valorHoraExtra){
+        this.valorHoraExtra = valorHoraExtra;
     }
 
     @Override
     public void calcularSalario(){
-        super.calcularSalario();
         double salarioFinal;
         salarioFinal = getSalario() + (valorHoraExtra * horaExtra);
+        System.out.println("Nome: " + getNome());
         System.out.println("Salário Final: " + salarioFinal);
+        System.out.println(" ");
     }
 }
